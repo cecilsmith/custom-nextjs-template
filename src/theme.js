6216aebf5f6ded1068bea790d47f8c6f3bd5b75f
cecilsmith <19243227+cecilsmith@users.noreply.github.com>
2022-01-18 16:05:56 -0600

    darkmode

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,7 @@ import { red } from '@mui/material/colors';
 // Create a theme instance.
 const theme = createTheme({
     palette: {
-        mode: "light",
+        mode: "dark",
         common: {
             black: '#212121',
             white: '#FAF8EB',
